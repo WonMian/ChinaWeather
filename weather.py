f77c9e8a26cf3d79588a204740c8dcbf260c7262
WonMian <478034625@qq.com>
2016-10-29 21:16:49 +0800

    Initial commit

---
+++ weather.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import requests
+import json
+import re
+from bs4 import BeautifulSoup
+import sys
+import os
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'}
+s = requests.session()
+fr = open('cityList.txt','r')
+
+
+def getCity(city):
+    cityDict = {}
+
+    lines = fr.readlines()
+    for line in lines:
+        list = line.split('=')
+        # cityDict[list[1].decode('utf-8')]=list[0]
+        if list[1].decode('utf-8') == city+' \r\n':
+            return list[0]
+    return None
+
+
+def getWeather():
+    choice = raw_input("查哪儿:\n")
+    code = getCity(choice)
+    if code:
+        url = 'http://www.weather.com.cn/data/cityinfo/%s.html'%str(code)
+        content = s.get(url,headers=headers).content
+        data = json.loads(content.decode())
+        result = data['weatherinfo']
+        str_temp = '%s %s %s ~ %s' % (result['city'],result['weather'],result['temp1'],result['temp2'])
+        return str_temp
+    else:
+        return '输入错误'
